04d9ea8bf6f26473a241c40e525b57950aaadfc9
anahita-karpour <anahita.karpour@gmail.com>
2020-04-22 22:00:33 +1200

    Programming I, assignment 2, partD-Master

--- GitHubFunctionTesting/src/com/anahitakarpour/PartD.java
+++ GitHubFunctionTesting/src/com/anahitakarpour/PartD.java
@@ -4,8 +4,7 @@ public class PartD {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//1- First change
-				System.out.println("Hi, my name is: Anahita Karpour");
+
 	}
 
 }
